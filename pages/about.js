13fbee6e8ada94a1847fb540adedc3afb2f5dae6
kevinhtran <kevdeveloper7@gmail.com>
2021-06-18 17:19:33 -0700

    adding parts of th enavbar

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>About</h1>
+      </main>
+    </div>
+  )
+}
+
+export default about
